9ffcfec3341deab668f00a2540c9f5103bc76bc8
chemical <48424568+chemical8263@users.noreply.github.com>
2021-10-19 22:51:37 +0900

    Model.meta,PlayerMove.csのコンフリクト解消

---
+++ Assets/Scripts/CannonShellMovement.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CannonShellMovement : MonoBehaviour
+{
+    int n = 0;
+    // Start is called before the first frame update
+
+    void Start()
+    {
+        Destroy(this.gameObject,5f);
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        var shellMovement = new Vector3(-0.01f, 0f, 0f);
+        transform.Translate(shellMovement);
+    }
+}
