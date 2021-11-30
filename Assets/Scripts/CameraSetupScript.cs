827fe3ddc5747236afb24d38ab38d79e1d7ff0e8
那裕 <kaminayu2714@gmail.com>
2021-11-30 23:33:34 +0900

    Cinemachine Targetgroup プレイヤー取得スクリプト　導入

---
+++ Assets/Scripts/CameraSetupScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cinemachine;
+
+public class CameraSetupScript : MonoBehaviour
+{
+    private CinemachineTargetGroup cinemachineTarget;
+    private GameObject[] players;
+
+    private void Start()
+    {
+        cinemachineTarget = GetComponent<CinemachineTargetGroup>();
+        players = GameObject.FindGameObjectsWithTag("Player");
+
+        foreach (GameObject p in players)
+        {
+            var t = p.transform;
+            cinemachineTarget.AddMember(t, 1.2f, 4);
+        }
+    }
+}
