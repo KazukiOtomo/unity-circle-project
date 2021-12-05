5389df555afd5374a1ed914afe3225d3d3f2eb88
那裕 <kaminayu2714@gmail.com>
2021-12-05 23:24:05 +0900

    Titleシーン構築 1回目

---
+++ Assets/Scripts/GameManagerScript.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManagerScript : MonoBehaviour
+{
+    private GameObject[] teamRed;
+    private GameObject[] teamBlue;
+
+    private void Start()
+    {
+        foreach(GameObject i in teamRed)
+        {
+
+        }
+    }
+}
