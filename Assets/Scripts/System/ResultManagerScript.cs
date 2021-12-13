a3be8ad5c9ff27c4ccfb8a304e906fa71d854ffc
那裕 <kaminayu2714@gmail.com>
2021-12-13 23:20:07 +0900

    Result シーン 微量構築 ＆ 針山 導入

---
+++ Assets/Scripts/System/ResultManagerScript.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ResultManagerScript : MonoBehaviour
+{
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape)) ReturnMainGameScene();
+    }
+
+    private void ReturnMainGameScene()
+    {
+        SceneManager.LoadScene("MainGame");
+    }
+}
