ec000d6f75f02dbfdb7bdd4efedbdd401d01f399
ga-zowmn <kuunesu.r@gmail.com>
2021-12-22 23:06:59 +0900

    落ちるやつ

---
+++ Assets/Falling.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Falling : MonoBehaviour
+{
+    public float FallSpeed = 1.0f;
+    public float Range = 1.0f;
+    public float DeleteTime = 3.0f;
+    // public GameObject needler;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Player = GameObject.FindGameObjectWithTag("Player");
+
+        // Update is called once per frame
+    }
+    void Update()
+    {
+
+            if ((GameObject.FindGameObjectWithTag("Player").transform.position.x < this.transform.position.x -Range && GameObject.FindGameObjectWithTag("Player").transform.position.y<this.transform.position.y) ||
+            (GameObject.FindGameObjectWithTag("Player").transform.position.x < this.transform.position.x +Range && GameObject.FindGameObjectWithTag("Player").transform.position.y < this.transform.position.y))
+            {
+                this.gameObject.transform.Translate(0, 0, FallSpeed);
+            Destroy(this.gameObject, DeleteTime);
+            }
+        
+    }
+    /*void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            
+            //Destroy(other.gameObject);
+            Destroy(this.gameObject);
+        }
+       // Destroy(this.gameObject,DeleteTime);
+    }*/
+}
---
+++ Assets/Scripts/PlayerMove.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayerMove : MonoBehaviour
+{
+	public float speed;
+
+	public AudioClip jump_SE;
+
+	AudioSource audio;
+	void Start()
+	{
+		audio = GetComponent<AudioSource>();
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		if (Input.GetKey("right"))
+		{
+			transform.position += transform.forward * speed * Time.deltaTime;
+		}
+		if (Input.GetKey("left"))
+		{
+			transform.position -= transform.forward * speed * Time.deltaTime;
+		}
+		if (Input.GetKey("up"))
+		{
+			transform.position += transform.up * speed * Time.deltaTime;
+
+			if (Input.GetKeyDown("up"))
+			{
+				audio.PlayOneShot(jump_SE);
+			}
+		}
+		if (Input.GetKey("down"))
+		{
+			transform.position -= transform.up * speed * Time.deltaTime;
+		}
+	}
+
+	private void OnCollisionEnter(Collision collision)
+	{
+		if (collision.gameObject.tag == "Finish")
+		{
+			SceneManager.LoadScene("Result");
+		}
+
+		if (collision.gameObject.tag == "Dead")
+        {
+			transform.position = new Vector3(-20f, 3f, 0f);
+			Destroy(collision.gameObject);
+        }
+	}
+}
